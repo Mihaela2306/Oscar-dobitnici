14ac2b2be87106dfb778899b50cc8cfc2139f334
Mihaela Čalogović <46655278+kraljtomislav@users.noreply.github.com>
2021-12-12 23:50:53 +0100

    Update dodajFilm.php

--- dodajFilm.php
+++ dodajFilm.php
@@ -7,11 +7,11 @@ if(isset($_GET["filmId"]) && isset($_GET["rating"])){
 	$rating = $_GET["rating"];
 	$sql = "SELECT * FROM filmovi WHERE id='" . $idFilma ."';";
 }else{
-	header("Location: ../PROJEKT/film.php?filmId=" . $idFilma . "&dodan=error");
+	header("Location: ../film.php?filmId=" . $idFilma . "&dodan=error");
 }
 $idKorisnika = $_SESSION["id"];
 
 $query = "INSERT INTO korisnik_film (korisnikId, filmId, rating) VALUES ('$idKorisnika', '$idFilma', '$rating')"; 
 $result = mysqli_query($conn, $query);
 
-header("Location: ../PROJEKT/film.php?dodan=success&filmId=" . $idFilma . "");
+header("Location: ../film.php?dodan=success&filmId=" . $idFilma . "");
