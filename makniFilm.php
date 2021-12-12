eff10866854b46c0969ee8232a3c1bde6765313a
Mihaela Čalogović <46655278+kraljtomislav@users.noreply.github.com>
2021-12-12 23:58:47 +0100

    Update makniFilm.php

--- makniFilm.php
+++ makniFilm.php
@@ -6,15 +6,15 @@ if(isset($_GET["filmId"])){
 	$idFilma = $_GET["filmId"];
 	$sql = "SELECT * FROM filmovi WHERE id='" . $idFilma ."';";
 }else{
-	header("Location: ../PROJEKT/film.php?filmId=" . $idFilma . "&promjena=error");
+	header("Location: ../film.php?filmId=" . $idFilma . "&promjena=error");
 }
 $idKorisnika = $_SESSION["id"];
 
 $query = "DELETE FROM korisnik_film WHERE korisnikId='" . $idKorisnika ."' AND filmId = '".$idFilma . "'";
 $result = mysqli_query($conn, $query);
 	if($result){
-		header("Location: ../PROJEKT/film.php?promjena=success&filmId=" . $idFilma . "");
+		header("Location: ../film.php?promjena=success&filmId=" . $idFilma . "");
 	} else {
-		header("Location: ../PROJEKT/film.php?filmId=" . $idFilma . "&promjena=error");
+		header("Location: ../film.php?filmId=" . $idFilma . "&promjena=error");
 	}
 
